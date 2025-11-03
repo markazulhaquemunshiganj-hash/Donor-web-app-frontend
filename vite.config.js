edc41d8700cc01c8bab772a8d74e62a54578a4c1
sohrab hosen <sohrabhosen16105@gmail.com>
2025-11-03 08:14:53 +0600

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 }
+  plugins: [
+    react(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      manifest: true,
+      workbox: {
+        cleanupOutdatedCaches: true,
+      },
+    }),
+  ],
+  server: { port: 5173 },
+
 });
