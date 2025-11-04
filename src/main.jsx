8d42855ee4667c4aadb7e635742c10eca90ce77c
sohrab hosen <sohrabhosen16105@gmail.com>
2025-11-04 21:02:20 +0600

    sir

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,8 @@ import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(<App />);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
